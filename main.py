4ad820db640293638b3b307d7d942048ed0b6607
CodeMike03 <114488938+CodeMike03@users.noreply.github.com>
2022-10-15 13:57:14 +0100

    main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+# Serwacki, Michal
+# Electronic Engineering student
+
+print("  S S S     M         M  ")
+print(" S      S   M M     M M  ")
+print(" S          M  M   M  M  ")
+print("  S S S     M    M    M  ")
+print("        S   M         M  ")
+print(" S      S   M         M  ")
+print("  S S S     M         M  ")
